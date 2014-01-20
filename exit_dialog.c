53b00ad3d222f4ab19caf32643f9e7a81acbfbbe
Andrew Reyes <dsreyes1014@gmail.com>
2014-01-20 12:38:08 -0600

    updates

--- exit_dialog.c
+++ exit_dialog.c
@@ -62,11 +62,13 @@ int main(int argc, char *argv[])
 {
 	GtkWidget *box_1,
 	          *box_2,
+	          *box_3,
 	          *label,
 	          *image_1,
 	          *image_2,
 	          *image_3,
 	          *image_4,
+	          *image_5,
 	          *window,
  	          *cancel_b,
  	          *restart_b,
@@ -75,9 +77,10 @@ int main(int argc, char *argv[])
  	
 	gtk_init(&argc, &argv); 	
  	
- 	label = gtk_label_new("Please choose");
-	box_1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20); 	
- 	box_2 = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+ 	label = gtk_label_new("Please choose an option");
+	box_1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 15); 	
+	box_2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 80);
+ 	box_3 = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
  	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	logout_b = gtk_button_new_with_mnemonic("_Logout"); 	
  	cancel_b = gtk_button_new_with_mnemonic("_Cancel");
@@ -87,10 +90,11 @@ int main(int argc, char *argv[])
 	image_2 = gtk_image_new_from_file("/usr/share/icons/Faenza/actions/24/system-log-out.png");
 	image_3 = gtk_image_new_from_file("/usr/share/icons/Faenza/actions/24/system-restart-panel.png");	
 	image_4 = gtk_image_new_from_file("/usr/share/icons/Faenza/actions/24/system-shutdown-panel-restart.png");
+	image_5 = gtk_image_new_from_file("/usr/share/icons/Faenza/actions/64/system-shutdown.png");
 	
 	gtk_window_set_title(GTK_WINDOW(window), "Exit");
 	
-	gtk_button_box_set_layout(GTK_BUTTON_BOX(box_2), GTK_BUTTONBOX_SPREAD);	
+	gtk_button_box_set_layout(GTK_BUTTON_BOX(box_3), GTK_BUTTONBOX_SPREAD);	
 	
 	gtk_button_set_image(GTK_BUTTON(cancel_b), image_1);
 	gtk_button_set_image(GTK_BUTTON(logout_b), image_2);	
@@ -107,13 +111,15 @@ int main(int argc, char *argv[])
 	gtk_button_set_always_show_image(GTK_BUTTON(restart_b), TRUE);	
 	gtk_button_set_always_show_image(GTK_BUTTON(shutdown_b), TRUE);	
 	
-	gtk_box_pack_start(GTK_BOX(box_1), label, FALSE, FALSE, 2);	
-	gtk_box_pack_start(GTK_BOX(box_1), box_2, FALSE, FALSE, 2);
+	gtk_box_pack_start(GTK_BOX(box_2), image_5, FALSE, FALSE, 2);	
+	gtk_box_pack_start(GTK_BOX(box_2), label, FALSE, FALSE, 2);
+	gtk_box_pack_start(GTK_BOX(box_1), box_2, FALSE,FALSE, 2);
+	gtk_box_pack_start(GTK_BOX(box_1), box_3, FALSE,FALSE, 2);
 	
-	gtk_container_add(GTK_CONTAINER(box_2), cancel_b);
-	gtk_container_add(GTK_CONTAINER(box_2), logout_b);
-	gtk_container_add(GTK_CONTAINER(box_2), restart_b);	
-	gtk_container_add(GTK_CONTAINER(box_2), shutdown_b);
+	gtk_container_add(GTK_CONTAINER(box_3), cancel_b);
+	gtk_container_add(GTK_CONTAINER(box_3), logout_b);
+	gtk_container_add(GTK_CONTAINER(box_3), restart_b);	
+	gtk_container_add(GTK_CONTAINER(box_3), shutdown_b);
 	gtk_container_add(GTK_CONTAINER(window), box_1);	
 	
 	g_signal_connect(window, "destroy", G_CALLBACK(close_window), NULL);
